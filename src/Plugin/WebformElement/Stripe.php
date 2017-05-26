931609255b2fb186d87baa09ec160f9050ec23ce
Ariel Barreiro <abarrei@gmail.com>
2017-05-26 18:41:27 -0300

    Initial work(ing) in progress

---
+++ src/Plugin/WebformElement/Stripe.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Drupal\stripe_webform\Plugin\WebformElement;
+
+use Drupal\webform\WebformElementBase;
+
+/**
+ * Provides a 'stripe' element.
+ *
+ * @WebformElement(
+ *   id = "stripe",
+ *   label = @Translation("Stripe element"),
+ *   category = @Translation("Stripe"),
+ *   description = @Translation("Provides a placeholder for a stripe elements integration."),
+ * )
+ */
+class Stripe extends  WebformElementBase {
+}
